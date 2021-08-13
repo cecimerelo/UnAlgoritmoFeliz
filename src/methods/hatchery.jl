2c73ca4b2978d5d11cd60dfac3e173e992853942
cecimerelo <ceci.merelo@gmail.com>
2021-08-13 21:45:10 +0200

    The hatchery was not properly dividing in castes
    - Now we will first stablish the population that will go in each caste
    and then sample it.

--- src/methods/hatchery.jl
+++ src/methods/hatchery.jl
@@ -8,12 +8,17 @@ function hatchery(population_model, population)
     percentages = [percentage for (caste, percentage) in population_model.config_parameters.castes_percentages]
     @assert(sum(percentages) == 100, "The percentages should have sum 100")
     @info "Dividing the population in castes"
+    caste_number_of_elements = Dict()
     castes = Dict()
 
     for (caste, percentage) in population_model.config_parameters.castes_percentages
-        @info "$(caste) -> $(percentage)%"
         elements_in_caste = floor(Int,(percentage * length(population)) / 100)
-        individuals_in_caste = sample(population, elements_in_caste; replace=true)
+        @info "$(caste) -> $(percentage)%, elements in caste -> $(elements_in_caste)"
+        caste_number_of_elements[caste] = elements_in_caste
+    end
+
+    for (caste, number_of_elements) in caste_number_of_elements
+        individuals_in_caste = sample(population, number_of_elements; replace=true)
         castes[caste] = individuals_in_caste
         delete_element_from_array(population, individuals_in_caste)
     end
--- src/test/methods/hatchery_test.jl
+++ src/test/methods/hatchery_test.jl
@@ -5,6 +5,19 @@ using .IndividualPackagesModule
 using StatsBase
 using Test
 
+
+function run_hatchery(config_file_path)
+    config_parameters_entity = utilsModule.read_parameters_file(config_file_path)
+    fitness_function = BlackBoxOptimizationBenchmarking.F1
+    population_model = PopulationModel(config_parameters_entity, fitness_function)
+    population = [
+        fertilising_room(population_model)
+        for _ in 1:population_model.config_parameters.population_size
+    ]
+    castes = hatchery(population_model, population)
+    return castes
+end
+
 @testset "Test hatchery when called then returns population divided in castes" begin
     @testset "Test when percentages sum 100" begin
         config_file_path = "./data/Config Files/config_file_1_test.json"
@@ -19,24 +32,13 @@ using Test
 
         population_in_castes = [length(caste_population) for (caste, caste_population) in castes]
         population_in_castes_sum = sum(population_in_castes)
-        @test population_in_castes_sum == length(population)
+        config_parameters_entity = utilsModule.read_parameters_file(config_file_path)
+
+        @test population_in_castes_sum == config_parameters_entity.population_size
     end
 
     @testset "Test when percentages sum 100" begin
         config_file_path = "./data/Config Files/config_file_2_test.json"
         @test_throws AssertionError("The percentages should have sum 100") run_hatchery(config_file_path)
     end
-
-end
-
-function run_hatchery(config_file_path)
-    config_parameters_entity = utilsModule.read_parameters_file(config_file_path)
-    fitness_function = BlackBoxOptimizationBenchmarking.F1
-    population_model = PopulationModel(config_parameters_entity, fitness_function)
-    population = [
-        fertilising_room(population_model)
-        for _ in 1:population_model.config_parameters.population_size
-    ]
-
-    castes = hatchery(population_model, population)
 end
