e36ca59d5d64ac4101e997dde42da346f3db95f4
ceciliamm <cecilia@badgermapping.com>
2021-08-21 22:35:58 +0200

    Repeat the crossover

--- src/methods/evolution.jl
+++ src/methods/evolution.jl
@@ -8,6 +8,13 @@ function evolution(population_in_castes, population_model)
     alpha_reproduction_pool = selector_operator(ALPHA(), population_in_castes[ALPHA()])
     beta_reproduction_pool = selector_operator(BETA(), population_in_castes[BETA()], alpha_reproduction_pool)
 
-    new_alpha_individuals = [crossover_operator(alpha_parents,population_model.config_parameters) for alpha_parents in alpha_reproduction_pool]    
-    new_beta_individuals = [crossover_operator(alpha_beta_parents, population_model.config_parameters) for alpha_beta_parents in beta_reproduction_pool]
+    new_alpha_individuals = [
+        crossover_operator(alpha_parents,population_model.config_parameters) 
+        for alpha_parents in alpha_reproduction_pool for _ in 1:2
+    ]    
+    new_beta_individuals = [
+        crossover_operator(alpha_beta_parents, population_model.config_parameters) 
+        for alpha_beta_parents in beta_reproduction_poolfor _ in 1:2
+    ]
+    
 end
