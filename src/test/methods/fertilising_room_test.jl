87b728827d2a9af2b42aae7cde16292393751664
ceciliamm <cecilia@badgermapping.com>
2021-08-29 11:28:03 +0200

    Fix tests

--- src/test/methods/fertilising_room_test.jl
+++ src/test/methods/fertilising_room_test.jl
@@ -10,7 +10,8 @@ config_file_path = "./src/test/Config Files/config_file_1_test.json"
 config_parameters_entity = read_parameters_file(config_file_path)
 fitness_function = BlackBoxOptimizationBenchmarking.F1
 range = (-5.12, 5.12)
-population_model = PopulationModel(config_parameters_entity, fitness_function, range)
+minimum_comparator = comparator(element, fitness_function) = element >= fitness_function.f_opt
+population_model = PopulationModel(config_parameters_entity, fitness_function, range, minimum_comparator)
 embryo = fertilising_room(population_model)
 
 @testset "Test fertilising_room when called then chromosome genes are in range" begin
