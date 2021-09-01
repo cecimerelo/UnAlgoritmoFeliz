e7c254c997b465345ab03952422d78b038049320
ceciliamm <cecilia@badgermapping.com>
2021-09-01 22:17:23 +0200

    Get diversity of the algorithm

---
+++ src/experiments/diversity_plots.jl
@@ -0,0 +1,50 @@
+using Gadfly
+using Cairo
+using Fontconfig
+
+outcomes = readdir("data/Outcomes")
+
+layers = []
+penColors   = distinguishable_colors( length(outcomes)+1 )
+
+for (i, file_name) in enumerate(outcomes)
+    if occursin("config_file_1", file_name)
+        files_complete_path = "./data/Outcomes/$(file_name)"
+        outcome_df = CSV.File(files_complete_path) |> DataFrame
+        @show outcome_df
+        layer = Gadfly.layer(
+                    outcome_df,
+                    x="Generations",
+                    y="F_Values",
+                    Geom.line,
+                    Theme(default_color=penColors[i+1])
+                )
+        push!(layers, layer)
+    end
+end
+
+p = Gadfly.plot(layers...)
+img = PNG("./data/Plots/config_file_1_Rastrigin.png", 6inch, 4inch)
+draw(img, p);
+
+best_f_value = CSV.File("./data/Outcomes/config_file_1_Rastrigin_14:34:33.csv") |> DataFrame
+
+p = vstack( 
+    Gadfly.plot(best_f_value, x="Generations", y="F_Values", Geom.line, Guide.ylabel("Fitness Value", orientation=:vertical)),
+    Gadfly.plot(best_f_value, x="Generations", y="Edit_distance", Geom.line, Guide.ylabel("Edit distance", orientation=:vertical)),
+    Gadfly.plot(best_f_value, x="Generations", y="Entropy", Geom.line, Guide.ylabel("Entropy", orientation=:vertical)),
+)
+
+img = PNG("./data/Plots/config_file_1_Rastrigin_best_f_value.png", 6inch, 6inch)
+draw(img, p)
+
+
+worst_f_value = CSV.File("./data/Outcomes/config_file_1_Rastrigin_14:29:02.csv") |> DataFrame
+
+p = vstack( 
+    Gadfly.plot(worst_f_value, x="Generations", y="F_Values", Geom.line, Guide.ylabel("Fitness Value", orientation=:vertical)),
+    Gadfly.plot(worst_f_value, x="Generations", y="Edit_distance", Geom.line, Guide.ylabel("Edit distance", orientation=:vertical)),
+    Gadfly.plot(worst_f_value, x="Generations", y="Entropy", Geom.line, Guide.ylabel("Entropy", orientation=:vertical)),
+)
+img = PNG("./data/Plots/config_file_1_Rastrigin_worst_f_value.png", 6inch, 6inch)
+draw(img, p)
